1c1f3c26935423667afd8c207eeddf3d7e119a68
p-lizarazo <pedritolizarazo@hotmail.com>
2017-09-04 19:46:31 -0500

    Commit

--- primsec.c
+++ primsec.c
@@ -3,13 +3,88 @@
     Santiago Lizarazo y Dorian Moreno
 */
 
-// Algoritmo usado: Criba de eratostenes
+// Algoritmo usado: Test de primalidad de Miller Rabin
 
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
-int list [100001]={1,1}; // Lista de booleanos para definir si un num es primo o no
+
+#define ITERATION 5 // Constante para reducir la probabilidad de fallo en el test probabilistico en 1/(4^n)
+
+/*
+ * calcula (a * b) % c tomando en cuenta que a*b puede salirse de los limites
+ * En particular en este programa se usa para calcular a^2 % mod para verificar test de primalidad
+ */
+
+long long mulmod(long long a, long long b, long long mod)
+{
+    long long x = 0,y = a % mod;
+    while (b > 0)
+    {
+        if (b % 2 == 1)
+        {
+            x = (x + y) % mod;
+        }
+        y = (y * 2) % mod;
+        b /= 2;
+    }
+    return x % mod;
+}
+/*
+ * Exponenciacion modular (mas rapida que una exponenciacion normal)
+ */
+long long modulo(long long base, long long exponent, long long mod)
+{
+    long long x = 1;
+    long long y = base;
+    while (exponent > 0)
+    {
+        if (exponent % 2 == 1)
+            x = (x * y) % mod;
+        y = (y * y) % mod;
+        exponent = exponent / 2;
+    }
+    return x % mod;
+}
+
+/*
+ * Test de primalidad Miller-Rabin , la iteración significa la probabilidad de fallo
+ */
+int Miller(long long p,int iteration)
+{
+
+    int i;
+    long long s;
+    if (p < 2)
+    {
+        return 0;
+    }
+    if (p != 2 && p % 2==0)
+    {
+        return 0;
+    }
+     s = p - 1;
+    while (s % 2 == 0)
+    {
+        s /= 2;
+    }
+    for (i = 0; i < iteration; i++)
+    {
+        long long a = rand() % (p - 1) + 1, temp = s;
+        long long mod = modulo(a, temp, p);
+        while (temp != p - 1 && mod != 1 && mod != p - 1)
+        {
+            mod = mulmod(mod, mod, p);
+            temp *= 2;
+        }
+        if (mod != p - 1 && temp % 2 == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
 
 // Main
 int main(int argc, char *argv[]){
@@ -40,17 +115,8 @@ int main(int argc, char *argv[]){
     }
     // Verificacion de datos - final
 
-    s_limit=(int)(sqrt(limit)); // Limite hasta la raiz del limite ya que no debe verificarse mas Ej( con 49 va hasta el 7)
-    for(i=2;i<=s_limit;i++){    // Agarrar el primer numero, considerado primo y tachar el resto hasta s_limit
-        if(list[i]==0){
-            for(j=i;i*j<=limit;j++){
-                list[i*j]=1;
-            }
-        }
-    }
-
-    for(i=0;i<limit;i++){   // Imprimir en el archivo los primos
-        if(!list[i]) fprintf(f,"%d, ",i);
+    for(i=2;i<limit;i++){   // Imprimir en el archivo los primos
+        if(Miller(i,ITERATION)) fprintf(f,"%d, ",i);
     }
     fclose(f);
     return 0;
